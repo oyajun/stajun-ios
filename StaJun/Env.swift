1d1e0dd0fd0643c12d7082123ad468c39e9895f5
oyajun <oyamadajun@outlook.jp>
2026-03-23 23:58:18 +0900

    Env.swift DEBUG

--- StaJun/Env.swift
+++ StaJun/Env.swift
@@ -5,8 +5,8 @@
 //  Created by 小山田純 on 2026/03/21.
 //
 
-#if DEVELOP
-let ENV_BASEURL = "http://192.168.128.145:3000/"
+#if DEBUG
+let ENV_BASEURL = "http://localhost:3000/"
 
 #else //PRODUCTION
 let ENV_BASEURL = "http://localhost:3000/"
